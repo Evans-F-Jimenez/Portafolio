e12b7cba04f6c40168887adcedcae4482ae7db65
Evans Jimenez <evansjimenez22@gmail.com>
2025-02-17 22:25:41 -0400

    Cambiar de lugar imagenes

--- src/data/education.js
+++ src/data/education.js
@@ -3,14 +3,12 @@ const educationData = [
         name: "Colegio Apec Fernando Arturo de Meriño",
         siglas: "CAFAM",
         period: "2015 - 2020",
-        icon: "./src/assets/cafam-icon.png"
+        icon: "/assets/cafam-icon.png"  // No "./src/" prefix
     },
     {
         name: "Instituto Tecnológico de Santo Domingo",
         siglas: "INTEC",
         period: "2021 - 2025",
-        icon: "./src/assets/intec-icon.png"
+        icon: "/assets/intec-icon.png"
     }
 ];
-
-export default educationData;
