4a5e86884002cc0ac3bc1c30534439c89cb6cbba
Evans Jimenez <evansjimenez22@gmail.com>
2025-02-17 20:47:41 -0400

    relativo para iconos de educacion

--- src/data/education.js
+++ src/data/education.js
@@ -3,13 +3,13 @@ const educationData = [
         name: "Colegio Apec Fernando Arturo de Meriño",
         siglas: "CAFAM",
         period: "2015 - 2020",
-        icon: "/src/assets/cafam-icon.png"
+        icon: "./src/assets/cafam-icon.png"
     },
     {
         name: "Instituto Tecnológico de Santo Domingo",
         siglas: "INTEC",
         period: "2021 - 2025",
-        icon: "/src/assets/intec-icon.png"
+        icon: "./src/assets/intec-icon.png"
     }
 ];
 
